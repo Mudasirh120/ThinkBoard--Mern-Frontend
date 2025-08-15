c0eb8be02b74a82f9eddd0f596edde93595a4903
Mudasirh120 <mudasirh120@gmail.com>
2025-08-15 20:14:14 +0500

    URL added in ENV file

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const api = axios.create({
-  baseURL: "https://think-board-mern-backend.vercel.app/",
+  baseURL: import.meta.env.VITE_API_BASE_URL,
 });
 export default api;
